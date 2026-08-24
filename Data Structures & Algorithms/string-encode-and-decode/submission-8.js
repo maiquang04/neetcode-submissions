cf0ede862a82a28bc6dff221d0bb4a76e02f6f6c
Quang Mai <104687048+maiquang04@users.noreply.github.com>
2026-08-24 12:41:46 +1000

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-8.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let res = "";
+        for (let str of strs) {
+            res += str.length + "#" + str;
+        }
+        return res;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        let res = [];
+        let i = 0;
+        while (i < str.length) {
+            let j = i;
+            while (str[j] !== "#") {
+                j++;
+            }
+            let len = +str.substring(i, j);
+            let s = str.substring(j + 1, j + 1 + len);
+            res.push(s);
+            i = j + 1 + len;
+        }
+        return res;
+    }
+}
